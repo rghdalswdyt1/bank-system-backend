20cbcb967c0da11cf6e62f9bd2fe117403cd334e
youssef dardeer <youssef.dardeer.mousa@gmail.com>
2024-10-18 21:23:02 +0300

    public cors

--- app.methods.js
+++ app.methods.js
@@ -9,12 +9,7 @@ dotenv.config();
 export const appMethods = (app, express) => {
     app.use(express.json());
     app.use(morgan("dev"));
-    app.use(cors({
-        origin: 'https://bank-system-website.vercel.app', // Allow only your frontend domain
-        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
-        credentials: true, // Allow credentials (if using cookies or authorization headers)
-      }));
-      
+    app.use(cors());
     app.use("/apis/customer", CustomerRouter);
     app.use("/apis/accountBank", AccountBankRouter);
     app.use("/apis/transaction", TransactionRouter);
